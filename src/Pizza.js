f9a9e7b708745e7ae5374ab1ba9996454f2a8807
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-16 16:03:54 -0400

    fixed error with checkbox filter in onSubmit().

--- src/Pizza.js
+++ src/Pizza.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import { Redirect, useHistory } from 'react-router-dom'
+import axios from "axios";
+import { Redirect, useHistory } from "react-router-dom";
 import * as yup from "yup";
 import schema from "./formSchema";
 import "./App.css";
@@ -17,50 +17,49 @@ const initFormValues = {
 };
 
 const initFormErrors = {
-    size: "",
-    sauce: "",
-}
+  size: "",
+  sauce: "",
+};
 
-
+const toppingsList = ["pepperoni", "sausage", "mushrooms", "olives"];
 
 const Pizza = () => {
   const [formValues, setFormValues] = useState(initFormValues);
   const [disabled, setDisabled] = useState(true);
-  const [formErrors, setFormErrors] = useState(initFormErrors)
+  const [formErrors, setFormErrors] = useState(initFormErrors);
   let history = useHistory();
 
   const confirm = () => {
-    
-      history.push('/confirmation')
-  }
+    // history.push("/confirmation");
+  };
 
   const postNewOrder = (newOrder) => {
-      axios
-        .post('https://reqres.in/api/users', newOrder)
-        .then(result => {
-            setFormValues(initFormValues)
-            alert('success!');
-            console.log(result);
-            confirm();
-        })
-  }
+    axios.post("https://reqres.in/api/users", newOrder).then((result) => {
+      setFormValues(initFormValues);
+      alert("success!");
+      console.log(result);
+      confirm();
+    });
+  };
 
   const onChange = (evt) => {
     const { name, value, type, checked } = evt.target;
     const valueToUse = type === "checkbox" ? checked : value;
     yup
-        .reach(schema, name)
-        .validate(value)
-        .then(() => {
-            setFormErrors({
-                ...formErrors, [name]: "",
-            })
-        })
-        .catch((err) => {
-            setFormErrors({
-                ...formErrors, [name]: err.errors[0]
-            })
-        })
+      .reach(schema, name)
+      .validate(value)
+      .then(() => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
     setFormValues({
       ...formValues,
       [name]: valueToUse,
@@ -72,11 +71,9 @@ const Pizza = () => {
     const newOrder = {
       size: formValues.size,
       sauce: formValues.sauce,
-      //   toppings: ["pepperoni", "sausage", "mushrooms", "olives"].filter(
-      //     (topping) => {
-      //       formValues[topping];
-      //     }
-      //   ),
+      toppings: toppingsList.filter(topping => {
+          return formValues[topping];
+      }),
       glutenFree: formValues.glutenFree,
       special: formValues.special,
     };
@@ -95,6 +92,10 @@ const Pizza = () => {
         <h2>Build Your Own Pizza!</h2>
       </div>
       <form className='form container' onSubmit={onSubmit}>
+        <div className='option-header'>
+          <h3>Choice of Size</h3>
+          <h4>Required</h4>
+        </div>
         <label className='form-label'>
           Size
           <select onChange={onChange} value={formValues.size} name='size'>
@@ -104,6 +105,10 @@ const Pizza = () => {
             <option value='14inch'>x-Large 14 Inch</option>
           </select>
         </label>
+        <div className='option-header'>
+          <h3>Choice of Sauce</h3>
+          <h4>Required</h4>
+        </div>
         <label>
           Red Pizza Sauce
           <input
@@ -134,6 +139,10 @@ const Pizza = () => {
             onChange={onChange}
           />
         </label>
+        <div className='option-header'>
+          <h3>Add Toppings</h3>
+          <h4>Choose up to 4</h4>
+        </div>
         <label>
           Pepperoni
           <input
@@ -170,6 +179,10 @@ const Pizza = () => {
             onChange={onChange}
           />
         </label>
+        <div className='option-header'>
+          <h3>Substitute</h3>
+          <h4>Optional</h4>
+        </div>
         <label>
           Gluten-Free
           <input
@@ -179,15 +192,21 @@ const Pizza = () => {
             onChange={onChange}
           />
         </label>
-        <label>
-          <input
-            value={formValues.special}
-            onChange={onChange}
-            name='special'
-            type='text'
-            placeholder='Anything else you would like to add?'
-          />
-        </label>
+        <div className='option-header'>
+          <h3>Special Instructions</h3>
+        </div>
+        <label htmlFor='special'></label>
+        <textarea
+          id='special'
+          value={formValues.special}
+          onChange={onChange}
+          name='special'
+          type='text'
+          placeholder='Anything else you would like to add?'
+          rows='4'
+          cols='40'
+        />
+
         <button className='submit' disabled={disabled}>
           Submit
         </button>
